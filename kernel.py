641df509f2bffd67bf77bc1b7635e6f3b2e8a9b1
Benjamin James <benjames@mit.edu>
2019-12-03 14:39:52 -0500

    cross validation along with feature selection

--- kernel.py
+++ kernel.py
@@ -1,5 +1,6 @@
 from sklearn.metrics.pairwise import polynomial_kernel
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 
 class PolyKernel(BaseEstimator, TransformerMixin):
@@ -7,11 +8,33 @@ class PolyKernel(BaseEstimator, TransformerMixin):
 		"""pdata is ENTIRE data set NS x NF"""
 		out = []
 		for r_x in pdata:
-			x = r_x * 2
-			pk = polynomial_kernel(x.astype(np.uint8).reshape(-1, 1)).flatten() * 2
-			print("shape:", x.shape, pk.shape)
-			out.append(np.concatenate((x.astype(np.uint8), pk), axis=None))
+			x = r_x
+			pk = polynomial_kernel(x.reshape(-1, 1)).flatten()
+			out.append(np.concatenate((x, pk), axis=None))
 		return np.asarray(out)
 
 	def fit(self, data, target):
 		return self
+
+def dump_prediction(Pred, Label, filename="out.txt"):
+	with open(filename, 'w') as F:
+		for p, l in zip(Pred, Label):
+			print(p, l, file=F)
+
+class Unscaler(BaseEstimator, TransformerMixin):
+	def __init__(self, scaler):
+		self.scaler = scaler
+		self.counter = 1
+	def fit(self, data, target):
+		return self
+	def transform(self, data):
+		return self.scaler.inverse_transform(data)
+	def scorer(self, estimator, X, y):
+		pred_y = estimator.predict(X)
+		pred_y[pred_y > 1] = 1
+		pred_y[pred_y < 0] = 0
+		t_pred_y = self.scaler.inverse_transform(pred_y.reshape(-1, 1))
+		t_y = self.scaler.inverse_transform(y.reshape(-1, 1))
+		dump_prediction(t_pred_y.flatten(), t_y.flatten(), filename="out%d.txt" % self.counter)
+		self.counter += 1
+		return -1 * np.mean(np.abs([(a - b) for a, b in zip(t_pred_y, t_y)]))
--- pipeline.py
+++ pipeline.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python3
 import sys, os
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics.pairwise import polynomial_kernel
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.linear_model import ElasticNet, LinearRegression
 from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import PolynomialFeatures
+
 from data import Data
-from kernel import PolyKernel
+from kernel import PolyKernel, Unscaler
+np.seterr(divide='ignore', invalid='ignore')
+
+def run(model_data, crossval=5, n_feat=10):
+	model = Pipeline([
+		("feat_select", SelectKBest(f_regression, k=n_feat)),
+#		("poly_kernel", PolynomialFeatures(degree=2)),
+		("regressor", LinearRegression())
+	])
+	scores = cross_val_score(model, model_data.feature_list,
+							 model_data.label_list.flatten(), cv=crossval,
+							 n_jobs=1,
+							 scoring=Unscaler(model_data.scaler).scorer)
+	out = np.asarray(scores)
+	print("all mae:", np.abs(out))
+	print("mae: %.2f +/- %.2f" % (np.abs(np.mean(out)), np.std(out)))
+	# with open('weights.txt', 'w') as F:
+	# 	print(' '.join(list(map(str, lr.coef_))), file=F)
 
 if __name__ == "__main__":
 	if len(sys.argv[1:]) != 1:
@@ -18,21 +38,4 @@ if __name__ == "__main__":
 		bin_size = int(sys.argv[1])
 		data = Data(bin_size=bin_size, chrom_sizes={"chr1": 249250621})#, "chr10": 135534747, "chr11": 135006516, "chr12": 133851895, "chr13": 115169878, "chr14": 107349540, "chr15": 102531392, "chr16": 90354753, "chr17": 81195210, "chr18": 78077248, "chr19": 59128983, "chr2": 243199373, "chr20": 63025520, "chr21": 48129895, "chr22": 51304566, "chr3": 198022430, "chr4": 191154276, "chr5": 180915260, "chr6": 171115067, "chr7": 159138663, "chr8": 146364022, "chr9": 141213431, "chrX": 155270560, "chrY": 59373566})#{"chr2": 243199373})
 		data.dump("data.pickle")
-		# poly(X) = (X^T * X / len(X) + c0)^d
-	X_train, X_test, y_train, y_test = train_test_split(
-		data.feature_list, data.label_list.flatten(), test_size=0.33, random_state=42)
-
-	print(np.asarray(X_train).shape, np.asarray(y_train).shape)
-	print(np.asarray(X_test).shape, np.asarray(y_test).shape)
-	lr = LinearRegression()
-	model = Pipeline([("kernel", PolyKernel()),
-					  ("regressor", lr)])
-	model.fit(X_train, y_train)
-	predicted = model.predict(X_test)
-	s_pred = data.scaler.inverse_transform([[p] for p in predicted])
-	s_y    = data.scaler.inverse_transform([[y] for y in y_test])
-	for a, b in zip(s_pred, s_y):
-		print(a, b)
-	with open('weights.txt', 'w') as F:
-		print(' '.join(list(map(str, lr.coef_))), file=F)
-	print("mae:", np.mean([np.abs(a - b) for a, b in zip(s_pred, s_y)]))
+	run(data)
